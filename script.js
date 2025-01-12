cc83e38ea1bc06000aca7312aaadb911f5e452bb
Rebe-nya <filip.slabinaf@gmail.com>
2025-01-12 15:37:06 +0100

    Square

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+//Links
 let x = document.getElementById("x");
 let y = document.getElementById("y");
-let btn = document.createElement("btn");
+let btn = document.getElementById("btn");
 
-const maxX = 50;
-const maxY = 50;
+//functions
+function square(x, y) {
+    if (x >= 0 && x <= 50){
+        console.log("true")
+        if (y >= 0 && y <= 50){
+            console.log("true")
+        }
+        else {
+            console.log("false")
+        }
+    }
+    else {
+        console.log("false")
+    }
+    return;
+}
+btn.addEventListener("click", function() {
+    square(x.value, y.value);
+});
